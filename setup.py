9e4da6e7b7011cd0903a1249650c2bc6a86e536e
Ariel Morelli <arielmorelli2@gmail.com>
2021-01-26 08:13:36 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+PLUGIN_NAME="cm_plugin_cart_api_exchange"
+
+setup(
+    name=PLUGIN_NAME,
+    packages=[PLUGIN_NAME],
+    version="0.0.1",
+    description="Enable automatic cart creation in DPLA Exchange account",
+    url="https://github.com/arielmorelli/cm_plugin_cart_api_exchange",
+)
